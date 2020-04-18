83fec81dfd8e5068eda7a256ef03240e4ffdda33
sameervermatf <63183174+sameervermatf@users.noreply.github.com>
2020-04-18 21:51:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,48 @@ provider "aws" {
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.11.0.0/16"
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "foobar" {
+  name                          = "sameerCTFromTFCloud"
+  s3_bucket_name                = "${aws_s3_bucket.foo.id}"
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "foo" {
+  bucket        = "sameer-tf-test-trail"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::sameer-tf-test-trail"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::sameer-tf-test-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
